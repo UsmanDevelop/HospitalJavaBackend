37cb4ec0a21a336c5e531516c76aac9442ae7e50
Usman <usmanmalik9680@gmail.com>
2026-02-19 00:14:12 +0500

    Add jwt Filter

---
+++ src/main/java/Hospital/Management/Hospital/Management/Security/JwtTokenFilter.java
@@ -0,0 +1,36 @@
+package Hospital.Management.Hospital.Management.Security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    private final AuthService authService;
+
+    public JwtTokenFilter(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        log.info("Incoming request: ${s}", request.getRequestURI());
+        String tokenHeader = request.getHeader("Authorization");
+
+        if(tokenHeader == null || !tokenHeader.startsWith("Bearer")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = tokenHeader.split("Bearer")[1];
+
+        String username = authService.getUsernameFromToken(token);
+
+    }
+}
